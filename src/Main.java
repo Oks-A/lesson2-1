94659b24773fa18f79331c1ac8e714bceecf003c
Oksana <oksanka-bud@rambler.ru>
2023-11-07 23:28:21 +0300

    bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int startBalance = 100; // Начальный счёт
+        int amount = 1100; // Сумма пополнения
+        int bonus;
+
+        if (amount > 1000) {
+            bonus = amount / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int balance = startBalance + amount + bonus;
+        System.out.println("Ваш баланс:" + balance + "руб. Бонус:" + bonus + "руб.");
+
+
     }
 }
